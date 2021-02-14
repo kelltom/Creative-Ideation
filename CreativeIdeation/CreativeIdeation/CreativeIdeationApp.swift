d00171a96b6cccade3aab8d5c2cb2b8f79f89603
Kellen Evoy <evoyk@sheridancollege.ca>
2021-02-14 16:25:23 -0500

    Initial Commit

---
+++ CreativeIdeation/CreativeIdeation/CreativeIdeationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreativeIdeationApp.swift
+//  CreativeIdeation
+//
+//  Created by Kellen Evoy on 2021-02-14.
+//
+
+import SwiftUI
+
+@main
+struct CreativeIdeationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
