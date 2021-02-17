eefd73ca5c165ed340ded8a75489fe510afff8fe
Kellen Evoy <evoyk@sheridancollege.ca>
2021-02-17 16:41:13 -0500

    Changed file structure

--- CreativeIdeation/CreativeIdeation/CreativeIdeationApp.swift
+++ CreativeIdeation/CreativeIdeation/CreativeIdeationApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CreativeIdeationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
---
+++ CreativeIdeation/CreativeIdeation/Screens/LoginView/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  CreativeIdeation
+//
+//  Created by Kellen Evoy on 2021-02-17.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+            
+    }
+}
