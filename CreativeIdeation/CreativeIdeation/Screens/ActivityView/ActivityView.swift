ec9986a31aa69fd9ea390d3d70b1d8ace0207c1c
Matt Marini <marinim@sheridancollege.ca>
2021-02-27 17:08:28 -0500

    added activity view and related views for sidebar and active users

---
+++ CreativeIdeation/CreativeIdeation/Screens/ActivityView/ActivityView.swift
@@ -0,0 +1,35 @@
+//
+//  ActivityView.swift
+//  CreativeIdeation
+//
+//  Created by Matthew Marini on 2021-02-27.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    
+    let columns = [GridItem(.adaptive(minimum: 900))]
+    
+    var body: some View {
+        ZStack{
+            // Whatever view we use for the canvas will be placed here, so that all other elements are placed above it on the zstack
+            
+            HStack{
+                ActivityToolbar()
+                Spacer()
+                VStack{
+                    ActiveMembers()
+                    Spacer()
+                }
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityView()
+    }
+}
---
+++ CreativeIdeation/CreativeIdeation/Views/Views/ActivityCanvas/ActiveMembers.swift
@@ -0,0 +1,32 @@
+//
+//  ActiveMembers.swift
+//  CreativeIdeation
+//
+//  Created by Matthew Marini on 2021-02-27.
+//
+
+import SwiftUI
+
+struct ActiveMembers: View {
+    var body: some View {
+        HStack{
+            ProfilePic()
+                .padding(8)
+            ProfilePic()
+                .padding(8)
+            ProfilePic()
+                .padding(8)
+        }
+        .background(Color.white)
+        .cornerRadius(20)
+        .clipped()
+        .shadow(color: .black, radius: 4, y: 4)
+        
+    }
+}
+
+struct ActiveMembers_Previews: PreviewProvider {
+    static var previews: some View {
+        ActiveMembers()
+    }
+}
