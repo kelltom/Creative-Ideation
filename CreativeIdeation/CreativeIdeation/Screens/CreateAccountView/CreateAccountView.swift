956565b27594c161c3eaa394e7722a0784d34440
Matt Marini <marinim@sheridancollege.ca>
2021-03-13 16:18:23 -0500

    add account creation and navigation to groupview on successful creation

--- CreativeIdeation/CreativeIdeation/Screens/CreateAccountView/CreateAccountView.swift
+++ CreativeIdeation/CreativeIdeation/Screens/CreateAccountView/CreateAccountView.swift
@@ -13,6 +13,8 @@ struct CreateAccountView: View {
     // Allows for popping view back to LogInView
     @Binding var showLogIn: Bool
     
+    @State private var actionState: Int? = 0
+    
     // These will eventually be put in a ViewModel
     @State var fullname: String = ""
     @State var emailAddress: String = ""
@@ -38,20 +40,22 @@ struct CreateAccountView: View {
                 
                 MenuTextField(title: "Enter your password", input: $password)
                 
-                //Create account button
-//                NavigationLink(destination: UserInfoView()) {
-//                    BigButton(title: "Create Account")
-//                }
-//                .padding(.top)
+                // Create Account Link
+                NavigationLink(destination: GroupView(), tag: 1, selection: $actionState) {
+                    EmptyView()
+                }
                 
                 Button {
                     // Add a new document with a generated ID
                     Auth.auth().createUser(withEmail: emailAddress, password: password)
-                    { (result, error) in
+                    { authResult, error in
                         if error != nil {
                             print(error?.localizedDescription ?? "Error creating account")
                         } else {
-                            print("success")
+                            print("Success")
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                self.actionState = 1
+                            }
                         }
                     }
                 } label: {
