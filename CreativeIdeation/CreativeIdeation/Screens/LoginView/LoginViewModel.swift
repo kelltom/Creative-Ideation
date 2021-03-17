3c4f9a36727aa3d9b7127d475824fe33536799ee
Kellen Evoy <evoyk@sheridancollege.ca>
2021-03-17 16:12:08 -0400

    Implemented ViewModel for LoginView, Removed DBService due to complications

--- CreativeIdeation/CreativeIdeation/Screens/LoginView/LoginViewModel.swift
+++ CreativeIdeation/CreativeIdeation/Screens/LoginView/LoginViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import Firebase
+
+final class LoginViewModel: ObservableObject {
+    
+    //private var dbService : DBService!
+    private var db = Firestore.firestore()
+    
+    @Published var user = User()
+    @Published var authSuccess = false
+    @Published var msg = ""
+    @Published var showBanner = false
+    
+    func authenticate() {
+        // async
+        Auth.auth().signIn(withEmail: user.email, password: user.password) { (result, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+                self.msg = error?.localizedDescription ?? ""
+                self.authSuccess = false
+                self.showBanner = true // signal UI to display a banner
+            } else {
+                print("Success")
+                self.msg = "Success"
+                self.authSuccess = true
+                self.showBanner = true // signal UI to display a banner
+            }
+        }
+    }
+    
+}
---
+++ CreativeIdeation/CreativeIdeation/Screens/LoginView/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  CreativeIdeation
+//
+//  Created by Kellen Evoy on 2021-03-17.
+//
+
+import Foundation
+
+struct User {
+    var id = ""
+    var name = ""
+    var email = ""
+    var password = ""
+}
