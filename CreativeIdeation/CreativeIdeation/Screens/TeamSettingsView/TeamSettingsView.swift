468a1cbd9d61a94039050e7717fb597281fc81d1
Vanessa <li147@sheridancollege.ca>
2021-03-17 15:59:51 -0400

    changed the color scheme

--- CreativeIdeation/CreativeIdeation/Screens/TeamSettingsView/TeamSettingsView.swift
+++ CreativeIdeation/CreativeIdeation/Screens/TeamSettingsView/TeamSettingsView.swift
@@ -9,49 +9,91 @@ import SwiftUI
 
 struct TeamSettingsView: View {
     var userName: String = "Kellen Evoy"
-    var description: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo"
+    var description: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    @State private var profanityFilter = true
     
     var body: some View {
-        VStack{
-            //team image here
-            GroupPic().padding()
-            
-            VStack(alignment: .leading ){
-                Text("Team Name").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack{
+                //team image here
+                GroupPic().padding()
                 
-                HStack{
-                    Text(userName)
-                     
-                    Button{
-                        //button functionality
-                    }label:{
-                        //button design
-                        TextEditButton()
+                VStack(alignment: .leading ){
+                    Text("Team Name")
+                        .font(.system(size: 20))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    
+                    HStack{
+                        Text(userName)
+                            .font(.system(size: 18))
+                        
+                        Spacer()
+                        
+                        Button{
+                            //button functionality
+                        }label:{
+                            //button design
+                            TextEditButton()
+                        }
+                    }
+                    
+                    Text("Description")
+                        .font(.system(size: 20))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    
+                    HStack{
+                        Text(description)
+                            .font(.system(size: 18))
+                        Spacer()
+                        Button{
+                            //button functionality
+                        }label:{
+                            //button design
+                            TextEditButton()
+                        }
                     }
                 }
+                .padding()
+                .frame(minWidth:100,  maxWidth: 650, maxHeight: 340, alignment: .leading)
+                .background(Color(.white))
+                .cornerRadius(10)
                 
-                Text("Desctiption").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                HStack{
-                    Text(description)
-                     
-                    Button{
-                        //button functionality
-                    }label:{
-                        //button design
-                        TextEditButton()
-                    }
-                }
             }
-            .padding()
-            .frame(minWidth:100,  maxWidth: 650, maxHeight: 350, alignment: .leading)
-            .background(Color(.lightGray))
-            .cornerRadius(10)
+            .frame(maxWidth: 700, maxHeight: 500, alignment: .center)
+            .background(Color("brandPrimary"))
+            .cornerRadius(20)
+            
+            VStack(alignment: .leading){
+                Text("Profanity Control")
+                    .font(.system(size: 20))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.gray)
+                    .padding()
+                
+                HStack{
+                    Toggle("Filter Specfic Wods", isOn: $profanityFilter)
+                        .padding()
+                        
+                }
+                
+                HStack{
+                    Text("Blocked Words List")
+                        .padding()
+                    Spacer()
+                    Button{
+                        //button functionality
+                    }label:{
+                        //button design
+                        TextEditButton()
+                    }
+                }
+               
+            }
+            .frame(maxWidth: 650, maxHeight: 300)
+            Divider().frame(maxWidth: 650)
             
         }
-        .frame(maxWidth: 700, maxHeight: 500, alignment: .center)
-        .background(Color(.gray))
-        .cornerRadius(20)
-        
+                
     }
 }
 
