d2e00ac0074e76a0d1574398707f484afbf7237b
Kellen Evoy <evoyk@sheridancollege.ca>
2021-03-18 15:58:04 -0400

    Major refactor of ViewModels, removed old ViewModels, replaced with a single VM for User transactions, fixed issue with animating banners

--- CreativeIdeation/CreativeIdeation/ViewModels/UserAccountViewModel.swift
+++ CreativeIdeation/CreativeIdeation/ViewModels/UserAccountViewModel.swift
@@ -1,24 +1,50 @@
 //
-//  CreateAccountViewModel.swift
+//  UserAccountViewModel.swift
 //  CreativeIdeation
 //
-//  Created by Kellen Evoy on 2021-03-17.
+//  Created by Kellen Evoy on 2021-03-18.
 //
 
 import Foundation
 import Firebase
+import SwiftUI
 
-final class CreateAccountViewModel: ObservableObject {
+final class UserAccountViewModel: ObservableObject {
     
-    //private var dbService : DBService!
+    //private var dbService : DBService! 
     private var db = Firestore.firestore()
     
     @Published var user = User()
+    
+    @Published var authSuccess = false
     @Published var createSuccess = false
     @Published var msg = ""
     @Published var showBanner = false
     
+    func authenticate() {
+        self.showBanner = false
+        // Authenticate user credentials
+        Auth.auth().signIn(withEmail: user.email, password: user.password) { (result, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+                self.msg = error?.localizedDescription ?? ""
+                self.authSuccess = false
+            } else {
+                print("Success")
+                self.msg = "Success"
+                self.authSuccess = true
+            }
+            
+            // Display result to View
+            withAnimation {
+                self.showBanner = true
+                self.delayAlert()
+            }
+        }
+    }
+    
     func createAccount() {
+        self.showBanner = false
         // Add new User to authenticated list
         Auth.auth().createUser(withEmail: user.email, password: user.password)
         { authResult, error in
@@ -26,7 +52,13 @@ final class CreateAccountViewModel: ObservableObject {
                 print(error?.localizedDescription ?? "Error creating account")
                 self.msg = error?.localizedDescription ?? "Error creating account"
                 self.createSuccess = false
-                self.showBanner = true
+                
+                // Display results to View
+                withAnimation {
+                    self.showBanner = true
+                    self.delayAlert()
+                }
+                
             } else {
                 print("Successfully created User auth")
                 
@@ -41,16 +73,29 @@ final class CreateAccountViewModel: ObservableObject {
                         print("Error adding document: \(err)")
                         self.msg = "Error adding document: \(err)"
                         self.createSuccess = false
-                        self.showBanner = true
                     } else {
                         print("Document added with ID: \(ref!.documentID)")
                         self.msg = "Account created successfully!"
                         self.createSuccess = true
+                    }
+                    
+                    // Display results to View
+                    withAnimation {
                         self.showBanner = true
+                        self.delayAlert()
                     }
                 }
             }
             
+        }
+    }
+    
+    // Tells View to stop showing banner after 4 seconds
+    private func delayAlert() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            withAnimation {
+                self.showBanner = false
+            }
         }
     }
     
