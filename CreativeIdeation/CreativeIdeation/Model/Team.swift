eba68ee3e6fed73ba94196c8ba7952c605956103
Kellen Evoy <evoyk@sheridancollege.ca>
2021-03-19 14:09:43 -0400

    Created TeamViewModel and Team model

---
+++ CreativeIdeation/CreativeIdeation/Model/Team.swift
@@ -0,0 +1,18 @@
+//
+//  Team.swift
+//  CreativeIdeation
+//
+//  Created by Kellen Evoy on 2021-03-19.
+//
+
+import Foundation
+
+struct Team {
+    var id = ""
+    var teamName = ""
+    var teamDescription = ""
+    var createdBy = ""
+    
+    var members: [String] = []
+    var admins: [String] = []
+}
---
+++ CreativeIdeation/CreativeIdeation/ViewModels/TeamViewModel.swift
@@ -0,0 +1,73 @@
+//
+//  TeamViewModel.swift
+//  CreativeIdeation
+//
+//  Created by Kellen Evoy on 2021-03-19.
+//
+
+import Foundation
+import Firebase
+import SwiftUI
+
+final class TeamViewModel: ObservableObject {
+    
+    private var db = Firestore.firestore()
+    let user = Auth.auth().currentUser
+    
+    @Published var teams: [Team] = []   // populated when navigating to HomeView
+    @Published var selectedTeam: Team?  // selected team in the sidebar
+    @Published var newTeam = Team()     // used by CreateTeamView
+    
+    @Published var createSuccess = false
+    @Published var msg = ""
+    @Published var showBanner = false
+    
+    func createTeam() {
+        // Attempt to save new Team to db
+        var ref: DocumentReference? = nil
+        let userId: String = user?.uid ?? ""
+        
+        if userId != "" {
+            
+            ref = db.collection("teams").addDocument(data: [
+                "teamName": self.newTeam.teamName,
+                "teamDescription": self.newTeam.teamDescription,
+                "createdBy": userId,
+                "admins": FieldValue.arrayUnion([userId]),
+                "members": FieldValue.arrayUnion([userId])
+            ]) { err in
+                if let err = err {
+                    print("Error adding document: \(err)")
+                    self.createSuccess = false
+                    self.msg = "Error creating team: \(err)"
+                    withAnimation {
+                        self.showBanner = true
+                        self.delayAlert()
+                    }
+                } else {
+                    // On success
+                    print("Document added with ID: \(ref!.documentID)")
+                }
+            }
+            
+        } else {
+            
+            self.msg = "Error: User ID not found"
+            self.createSuccess = false
+            withAnimation {
+                self.showBanner = true
+                self.delayAlert()
+            }
+        }
+    }
+        
+    // Tells View to stop showing banner after 4 seconds
+    private func delayAlert() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            withAnimation {
+                self.showBanner = false
+            }
+        }
+    }
+    
+}
