732060b39887c741bfa3ed59f65a3b26c67e6b91
Matt Marini <marinim@sheridancollege.ca>
2021-07-06 15:17:21 -0400

    add IDs to member class for ease of filtering with groups

--- CreativeIdeation/CreativeIdeation/Model/Member.swift
+++ CreativeIdeation/CreativeIdeation/Model/Member.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 struct Member: Codable, Identifiable, Hashable {
-    let id = UUID()
+    let memberId = UUID()
+    let id: String = ""
     let name: String = ""
     let email: String = ""
 
     enum CodingKeys: String, CodingKey {
+        case id
         case name
         case email
     }
