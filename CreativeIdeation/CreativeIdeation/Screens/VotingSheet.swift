ab618e6ad017dc8dd303aed967611d8ce8adb769
Matt Marini <marinim@sheridancollege.ca>
2021-08-16 14:01:58 -0400

    begin implementation of voting system by adding score, haveVoted, and isVoting variables to session and sessionItems

---
+++ CreativeIdeation/CreativeIdeation/Screens/VotingSheet.swift
@@ -0,0 +1,20 @@
+//
+//  VotingSheet.swift
+//  CreativeIdeation
+//
+//  Created by Matthew Marini on 2021-08-16.
+//
+
+import SwiftUI
+
+struct VotingSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VotingSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        VotingSheet()
+    }
+}
